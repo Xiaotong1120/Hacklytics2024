41e72495a9cac4113b34cc4a02c43d4d51417ff7
Xiaotong Ma <56909353+Xiaotong1120@users.noreply.github.com>
2024-02-11 02:47:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import io
 import os
 import re
+import openai
+import pandas as pd
 import gradio as gr
 import matplotlib.pyplot as plt
+from Hacklytics2024.Handwriting_recognition import *
+from Hacklytics2024.Output_Generation import *
 
 # Define functions for each page
 def home():
@@ -25,8 +29,6 @@ def home():
     """
     return content
 
-text_box = gr.Textbox(label="Learning Assistant Response", lines=10)
-
 def main():
     with gr.Blocks() as app:
         with gr.Tab("Home"):
@@ -34,21 +36,27 @@ def main():
 
         with gr.Tab("AI-Assisted Learning"):
             with gr.Column():
-                file_input = gr.File(label="Upload a problem!")
-                upload_button = gr.Button("Upload")
-                text_output = gr.Textbox(label="AI Output", lines=10)
-
-            # with gr.Column():
-            #     upload_button.click(inputs=file_input, outputs=[text_output])
-
-            with gr.Row():
-                inputs = text_output,
-                outputs = "text",
-                title = "Learning Assistant Response"
+                file_input = gr.File(label="Upload an image of problem!")
+                submit_button = gr.Button("Submit")
+                text_output = gr.Textbox(label="AI Output")
 
             with gr.Row():
                 check_button = gr.Button("I understand!")
                 confused_button = gr.Button("I'm a bit confused...")
+
+            convert_img_to_text, output_generation, continue_exploring, memory_initial = m4th_assistant()
+            
+            # convert img to text
+            file_input.change(convert_img_to_text, inputs = file_input)
+
+            # calling Chatgpt API
+            submit_button.click(output_generation, outputs = text_output) 
+
+            # continuing asking, calling Chatgpt, concatenating history data
+            confused_button.click(continue_exploring, outputs = text_output)
+
+            # nothing more to learn, clear all
+            check_button.click(memory_initial, outputs = text_output)
 
     app.launch()
 
